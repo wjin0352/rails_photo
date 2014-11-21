103e7a356cc3c11bec16ab7ba09e91c6d46b4ed1
won <wjin@nyit.edu>
2014-11-21 16:25:57 -0500

    rails photo project

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ActiveRecord::Base
+  # Mounts the given uploader on the given column.
+  #This means that assigning and reading from the column will upload and retrieve files.
+  mount_uploader :avatar,AvatarUploader
+end
---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :title, :avatar, :body, :created_at, :updated_at
